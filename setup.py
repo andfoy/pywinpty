c77c4ef14f35628b5a3896002059b56ff4107123
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-04 16:36:55 -0500

    Added Cython setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, Extension
+# from distutils.extension import Extension
+from Cython.Build import cythonize
+# import os.path as osp
+
+setup(
+    ext_modules=cythonize([
+        Extension("winpty", ["winpty/winpty.pyx"], libraries=["winpty"])
+    ]),
+)
---
+++ winpty/winpty.pyx
@@ -0,0 +1,60 @@
+
+cimport cython
+from _winpty cimport winpty, winpty_constants
+
+cdef enum AgentConstants:
+   WINPTY_FLAG_CONERR = winpty_constants._WINPTY_FLAG_CONERR
+   WINPTY_FLAG_PLAIN_OUTPUT = winpty_constants._WINPTY_FLAG_PLAIN_OUTPUT
+   WINPTY_FLAG_COLOR_ESCAPES = winpty_constants._WINPTY_FLAG_COLOR_ESCAPES
+   WINPTY_FLAG_ALLOW_CURPROC_DESKTOP_CREATION = winpty_constants._WINPTY_FLAG_ALLOW_CURPROC_DESKTOP_CREATION
+
+
+cdef class Agent:
+    cdef winpty.winpty_t* _c_winpty_t
+    cdef HANDLE _agent_process
+    cdef LPCWSTR _conin_pipe_name
+    cdef LPCWSTR _conout_pipe_name
+    cdef LPCWSTR _conerr_pipe_name
+
+    def __cinit__(self, int cols, int rows,
+                  int mouse_mode=winpty_constants._WINPTY_MOUSE_MODE_AUTO,
+                  int timeout=3000, int agent_config=winpty_constants._WINPTY_FLAG_MASK):
+        cdef winpty.winpty_error_t err_pointer
+        cdef winpty.winpty_config_t* config = winpty.winpty_config_new(agent_config, err_pointer)
+
+        if config is NULL:
+            raise MemoryError(winpty.winpty_error_msg(err_pointer))
+
+        if err is not NULL:
+            msg = 'An error has ocurred: {0} - Code: {1}'.format(
+                winpty.winpty_error_msg(err_pointer),
+                winpty.winpty_error_code(err_pointer))
+            winpty.winpty_error_free(err_pointer)
+            raise RuntimeError(msg)
+
+        winpty.winpty_config_set_initial_size(config, cols, rows)
+        winpty.winpty_config_set_mouse_mode(config, mouse_mode)
+        winpty.winpty_config_set_agent_timeout(config, timeout)
+
+        err_pointer = NULL
+        self._c_winpty_t = winpty.winpty_open(config, err_pointer)
+        winpty.winpty_config_free(config)
+
+        if err_pointer is not NULL:
+            msg = 'An error has ocurred: {0} - Code: {1}'.format(
+                winpty.winpty_error_msg(err_pointer),
+                winpty.winpty_error_code(err_pointer))
+            winpty.winpty_error_free(err_pointer)
+            raise RuntimeError(msg)
+
+        self._agent_process = winpty.winpty_agent_process(self._c_winpty_t)
+        self._conin_pipe_name = winpty.winpty_conin_name(self._c_winpty_t)
+        self._conout_pipe_name = winpty.winpty_conout_name(self._c_winpty_t)
+        self._conerr_pipe_name = winpty.winpty_conerr_name(self._c_winpty_t)
+
+    def __dealloc__(self):
+        if self._c_winpty_t is not NULL:
+            winpty.winpty_free(self._c_winpty_t)
+
+
+
