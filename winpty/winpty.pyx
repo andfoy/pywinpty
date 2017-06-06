9802f816a2805263df44a2b591477d05f78b3b9f
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-06 00:13:29 -0500

    Writing char pointer instead of uchar

--- winpty/winpty.pyx
+++ winpty/winpty.pyx
@@ -61,7 +61,7 @@ ctypedef unsigned char UCHAR
 
 ctypedef struct OVLP:
     OVERLAPPED readOvlp
-    UCHAR* buf
+    UCHAR buf[8096]
 
 cdef void callback(DWORD err, DWORD bytes, LPVOID ovlp):
     cdef OVLP* temp = <OVLP*> ovlp
@@ -155,10 +155,10 @@ cdef class Agent:
                        &length, NULL)
         return buf
 
-    def read(self, int length=4096, DWORD timeout=1000):
+    def read(self, int length=1000, DWORD timeout=1000):
         cdef OVLP ovlp_read
-        ovlp_read.buf = <UCHAR*>calloc(length, sizeof(UCHAR))
-        cdef bint ret = ReadFileEx(self._conout_pipe, ovlp_read.buf, length,
+        # ovlp_read.buf = <UCHAR*>calloc(length, sizeof(UCHAR))
+        cdef bint ret = ReadFileEx(self._conout_pipe, ovlp_read.buf, sizeof(ovlp_read.buf),
                                    <LPOVERLAPPED>(&ovlp_read), callback)
         cdef DWORD status = SleepEx(timeout, True)
         cdef UCHAR* lines = ''
