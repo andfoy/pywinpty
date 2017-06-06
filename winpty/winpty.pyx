43fe266fbba4920705f0d1a274bce2c4ab83d093
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-05 23:33:01 -0500

    Fixed bufsize

--- winpty/winpty.pyx
+++ winpty/winpty.pyx
@@ -61,7 +61,7 @@ ctypedef unsigned char UCHAR
 
 ctypedef struct OVLP:
     OVERLAPPED readOvlp
-    UCHAR* buf
+    UCHAR buf[4096]
 
 cdef void callback(DWORD err, DWORD bytes, LPVOID ovlp):
     cdef OVLP* temp = <OVLP*> ovlp
@@ -157,7 +157,7 @@ cdef class Agent:
 
     def read(self, int length=1000, DWORD timeout=1000):
         cdef OVLP ovlp_read
-        ovlp_read.buf = <UCHAR*>calloc(length, sizeof(UCHAR))
+        # ovlp_read.buf = <UCHAR*>calloc(length, sizeof(UCHAR))
         cdef bint ret = ReadFileEx(self._conout_pipe, ovlp_read.buf, sizeof(ovlp_read.buf),
                                    <LPOVERLAPPED>(&ovlp_read), callback)
         cdef DWORD status = SleepEx(timeout, True)
