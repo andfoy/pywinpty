8d76842c3de7e51fb14d5684cc118ecb542fffed
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-07 22:43:50 -0500

    Clearing buffer up to penultimate character

--- winpty/winpty.pyx
+++ winpty/winpty.pyx
@@ -66,9 +66,9 @@ ctypedef struct OVLP:
 
 cdef void callback(DWORD err, DWORD in_bytes, LPVOID ovlp):
     cdef OVLP* temp = <OVLP*> ovlp
-    # cdef UCHAR* buf = temp.buf
-    # if in_bytes < 8096:
-    #     buf[in_bytes] = '\0'
+    cdef UCHAR* buf = temp.buf
+    if in_bytes < 8096:
+        buf[in_bytes - 1] = '\0'
 
 cdef class Agent:
     cdef winpty.winpty_t* _c_winpty_t
