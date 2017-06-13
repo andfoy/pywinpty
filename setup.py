c80f74a5e91c6d958bfcf4506d63ab4592cb9e71
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2017-06-13 14:33:12 -0500

    Add setup.py info

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from Cython.Build import cythonize
 # import os.path as osp
 
 setup(
+    name='pywinpty',
+    version='1.0',
     ext_modules=cythonize([
         Extension("winpty", sources=["winpty/winpty.pyx"],
                   libraries=["winpty"])
