26c6757a0c76f5b9d711712f2efacf32d9b53afa
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2017-06-23 15:56:35 -0500

    Remove uneeded try/except

--- winpty/__init__.py
+++ winpty/__init__.py
@@ -5,11 +5,14 @@ Pywinpty
 This package provides a Cython wrapper around winpty C++ library.
 """
 
-try:
-    from .winpty_wrapper import PTY
-    PTY
-except ImportError:
-    pass
+# yapf: disable
 
+# Local imports
+from .winpty_wrapper import PTY
+
+
+# yapf: enable
+
+PTY
 VERSION_INFO = (0, 1, 1, 'dev0')
 __version__ = '.'.join(map(str, VERSION_INFO))
