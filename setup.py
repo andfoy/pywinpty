13b9ab9b226c3baf1edae61a32a366ca21b731cf
Steven Silvester <steven.silvester@ieee.org>
2017-11-27 17:27:39 -0600

    Specify wheel package_data files

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
     description='Python bindings for the winpty library',
     ext_modules=ext_modules,
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    package_data=dict(pywinpty=['*.pyd', '*.dll', '*.exe']),
     setup_requires=['Cython'],
     install_requires=['backports.shutil_which;python_version<"3.0"'],
     classifiers=[
