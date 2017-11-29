36f4bf70d249eb06e01936d9826ee73f6803bb5d
Steven Silvester <steven.silvester@ieee.org>
2017-11-29 03:23:20 -0600

    Add trove classifier and note about VS Runtime

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
         'Development Status :: 4 - Beta', 'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ]
