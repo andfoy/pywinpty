0c66c743ce4a234b6dae2fec444f17c8a85833ae
Steven Silvester <steven.silvester@ieee.org>
2018-02-06 14:27:24 -0600

    address review

--- winpty/winpty_wrapper.py
+++ winpty/winpty_wrapper.py
@@ -15,9 +15,9 @@ import os
 import sys
 
 # Refuse to import on Windows 7
-if os.name == 'nt' and sys.getwindowsversion().major == 7:
-    raise OSError('Cannot use winpty on Windows 7, '
-                  'see https://github.com/spyder-ide/pywinpty/issues/59')
+if os.name == 'nt' and sys.getwindowsversion().major <= 7:
+    raise ImportError('Cannot use winpty on Windows 7, '
+                      'see https://github.com/spyder-ide/pywinpty/issues/59')
 
 
 PY2 = sys.version_info[0] == 2
