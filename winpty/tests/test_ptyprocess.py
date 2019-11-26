d8af843affb06dc9cc0367402a552e77b26e5697
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-11-26 14:39:58 -0500

    Migrate tests to new pytest

--- winpty/tests/test_ptyprocess.py
+++ winpty/tests/test_ptyprocess.py
@@ -16,15 +16,16 @@ import pytest
 
 # yapf: enable
 
-
 @pytest.fixture(scope='module')
-def pty_fixture(cmd=None, env=None):
-    cmd = cmd or 'cmd'
-    return PtyProcess.spawn(cmd, env=env)
+def pty_fixture():
+    def _pty_factory(cmd=None, env=None):
+        cmd = cmd or 'cmd'
+        return PtyProcess.spawn(cmd, env=env)
+    return _pty_factory
 
 
 @flaky(max_runs=4, min_passes=1)
-def test_read():
+def test_read(pty_fixture):
     pty = pty_fixture()
     loc = os.getcwd()
     data = ''
@@ -33,7 +34,7 @@ def test_read():
     pty.terminate()
 
 
-def test_write():
+def test_write(pty_fixture):
     pty = pty_fixture()
 
     text = u'Eggs, ham and spam ünicode'
@@ -45,7 +46,7 @@ def test_write():
     pty.terminate()
 
 
-def test_isalive():
+def test_isalive(pty_fixture):
     pty = pty_fixture()
     pty.write('exit\r\n')
 
@@ -64,7 +65,7 @@ def test_isalive():
     pty.terminate()
 
 
-def test_readline():
+def test_readline(pty_fixture):
     env = os.environ.copy()
     env['foo'] = 'bar'
     pty = pty_fixture(env=env)
@@ -76,69 +77,69 @@ def test_readline():
     pty.terminate()
 
 
-def test_close():
+def test_close(pty_fixture):
     pty = pty_fixture()
     pty.close()
     assert not pty.isalive()
 
 
-def test_flush():
+def test_flush(pty_fixture):
     pty = pty_fixture()
     pty.flush()
     pty.terminate()
 
 
-def test_intr():
+def test_intr(pty_fixture):
     pty = pty_fixture(cmd=[sys.executable, 'import time; time.sleep(10)'])
     pty.sendintr()
     assert pty.wait() != 0
 
 
-def test_send_control():
+def test_send_control(pty_fixture):
     pty = pty_fixture(cmd=[sys.executable, 'import time; time.sleep(10)'])
     pty.sendcontrol('d')
     assert pty.wait() != 0
 
 
-def test_send_eof():
+def test_send_eof(pty_fixture):
     cat = pty_fixture('cat')
     cat.sendeof()
     assert cat.wait() == 0
 
 
-def test_isatty():
+def test_isatty(pty_fixture):
     pty = pty_fixture()
     assert pty.isatty()
     pty.terminate()
     assert not pty.isatty()
 
 
-def test_wait():
+def test_wait(pty_fixture):
     pty = pty_fixture(cmd=[sys.executable, '--version'])
     assert pty.wait() == 0
 
 
-def test_exit_status():
+def test_exit_status(pty_fixture):
     pty = pty_fixture(cmd=[sys.executable])
     pty.write('import sys;sys.exit(1)\r\n')
     pty.wait()
     assert pty.exitstatus == 1
 
 
-def test_kill():
+def test_kill(pty_fixture):
     pty = pty_fixture()
     pty.kill(signal.SIGTERM)
     assert not pty.isalive()
     assert pty.exitstatus == signal.SIGTERM
 
 
-def test_getwinsize():
+def test_getwinsize(pty_fixture):
     pty = pty_fixture()
     assert pty.getwinsize() == (24, 80)
     pty.terminate()
 
 
-def test_setwinsize():
+def test_setwinsize(pty_fixture):
     pty = pty_fixture()
     pty.setwinsize(50, 110)
     assert pty.getwinsize() == (50, 110)
