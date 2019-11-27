4cd9112a7bddc4fea4b03d595f56b7940f9acbf8
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2019-11-26 20:47:19 -0500

    Syntax error correction

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -5,7 +5,7 @@ conda install -q --file conda.txt
 # Install distribution and coverage tools
 pip install -q twine codecov
 
-if($env:PYTHON_VERSION -match "2.7") {}
+if($env:PYTHON_VERSION -match "2.7") {
     # On Python 2, we need to install this dependency
     conda install -q backports.shutil_which
 }
