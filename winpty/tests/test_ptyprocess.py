9050d80122769b788400b81e5e54cda152a4e3a2
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2022-01-21 18:28:15 -0500

    Use winpty-rs 0.3.1

--- winpty/tests/test_ptyprocess.py
+++ winpty/tests/test_ptyprocess.py
@@ -29,7 +29,7 @@ def pty_fixture(request):
 
 def test_read(pty_fixture):
     pty = pty_fixture()
-    loc = os.getcwd() if os.environ.get('CI', None) is None else 'cmd'
+    loc = os.getcwd()
     data = ''
     while loc not in data:
         data += pty.read()
